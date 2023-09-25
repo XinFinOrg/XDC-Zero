30f87fd4dd5b50ff6560f0003b956170088fc8b5
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2023-09-25 12:56:39 +0400

    update

--- contracts/Endpoint.sol
+++ contracts/Endpoint.sol
@@ -1,10 +1,21 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity =0.8.19;
 
 import {IFullCheckpoint} from "./interfaces/IFullCheckpoint.sol";
 
 contract Endpoint {
 
+    struct Config {
+        IFullCheckpoint checkpoint;
+    }
 
-    
+    Config private _config;
+
+    uint256 private _initialized;
+
+    function initializer(Config calldata config) external {
+        require(_initialized == 0, "already initialized");
+        _config = config;
+        _initialized = 1;
+    }
 }
