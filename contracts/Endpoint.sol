786a6c41f9fc9a3710043b03474a1a50dc5f5f91
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2023-09-25 14:34:07 +0400

    update

--- contracts/Endpoint.sol
+++ contracts/Endpoint.sol
@@ -2,20 +2,48 @@
 pragma solidity =0.8.19;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import {IFullCheckpoint} from "./interfaces/IFullCheckpoint.sol";
+import {RLPEncode} from "./libraries/RLPEncode.sol";
 
 contract Endpoint is Ownable {
     struct Config {
         IFullCheckpoint checkpoint;
     }
 
+    struct TransactionProof {
+        bytes32 blockhash;
+        bytes32[] proof;
+        //receipt
+        bytes postState;
+        uint256 status;
+        uint256 cumulativeGasUsed;
+        bytes bloom;
+        bytes[] logs;
+        bytes txHash;
+        address contractAddress;
+        uint256 gasUsed;
+        //paylaod
+        bytes payload;
+    }
+
     Config private _config;
+
+    mapping(bytes => bytes) private _payloads;
 
     function send() external {}
 
-    function receivePayload() external {}
+    function receivePayload() external returns (bytes memory) {}
 
-    function validateTransactionProof() external {}
+    function validateTransactionProof(
+        TransactionProof calldata txProof
+    ) external {
+        IFullCheckpoint checkpoint = _config.checkpoint;
+        bytes32 root = checkpoint.getReceiptHash(txProof.blockhash);
+        bytes32 leaf = getLeaf(txProof);
+        require(MerkleProof.verify(txProof.proof, root, leaf), "invalid proof");
+        _payloads[txProof.txHash] = txProof.payload;
+    }
 
     function getConfig() external view returns (Config memory) {
         require(
@@ -31,5 +59,32 @@ contract Endpoint is Ownable {
             "invalid checkpoint"
         );
         _config = config;
+    }
+
+    function getLeaf(
+        TransactionProof memory txProof
+    ) public pure returns (bytes32) {
+        bytes[] memory encodedParts = new bytes[](8);
+
+        encodedParts[0] = RLPEncode.encodeBytes(txProof.postState);
+        encodedParts[1] = RLPEncode.encodeUint(txProof.status);
+        encodedParts[2] = RLPEncode.encodeUint(txProof.cumulativeGasUsed);
+        encodedParts[3] = RLPEncode.encodeBytes(txProof.bloom);
+        encodedParts[4] = RLPEncode.encodeList(txProof.logs);
+        encodedParts[5] = RLPEncode.encodeBytes(txProof.txHash);
+        encodedParts[6] = RLPEncode.encodeAddress(txProof.contractAddress);
+        encodedParts[7] = RLPEncode.encodeUint(txProof.gasUsed);
+
+        return bytesToBytes32(RLPEncode.encodeList(encodedParts));
+    }
+
+    function bytesToBytes32(bytes memory input) public pure returns (bytes32) {
+        require(input.length <= 32, "Input too long");
+
+        bytes32 output;
+        assembly {
+            output := mload(add(input, 32))
+        }
+        return output;
     }
 }
