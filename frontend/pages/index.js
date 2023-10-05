f3f6e5cdef60ed67b7338b67e7056830b1064f8d
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2023-10-05 13:37:22 +0400

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,13 +5,39 @@ export default function Home() {
 
   return (
     <>
-      <div className="card shadow-2xl w-[1000px] m-auto mt-20">
+      <div className="card shadow-2xl w-[1000px] m-auto mt-8">
         <div className="card-body">
-          <div className="font-black">Enpoint :{} </div>
+          <div className="font-black">Local ChainId : {1000} </div>
+          <div className="font-black">Local Enpoint : {"0x..."}</div>
+          <div className="card-actions justify-end">
+            <label className="btn btn-success w-max btn-sm">
+              Register a Chain
+            </label>
+          </div>
+
           <div className="divider"></div>
           <div className="card shadow-2xl">
             <div className="card-body">
-              <div className="font-black">Enpoint :{}</div>
+              <div className="font-black">Remote ChainId : {2}</div>
+              <div className="font-black">Local CSC : {"0x..."}</div>
+              <div className="font-black">Remote Enpoint : {"0x..."}</div>
+              <div className="card-actions justify-end">
+                <label className="btn btn-warning w-max btn-sm">
+                  Configration
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="card shadow-2xl">
+            <div className="card-body">
+              <div className="font-black">Remote ChainId : {13}</div>
+              <div className="font-black">Local CSC : {"0x..."}</div>
+              <div className="font-black">Remote Enpoint : {"0x..."}</div>
+              <div className="card-actions justify-end">
+                <label className="btn btn-warning w-max btn-sm">
+                  Configration
+                </label>
+              </div>
             </div>
           </div>
         </div>
