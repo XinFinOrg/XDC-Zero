df099a06c2180732498d56dd72048bb3082decaa
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-01-15 23:51:34 +0800

    add subswap code  (#7)
    add subswap code

---
+++ applications/subswap/contract/scripts/parennettreasurydeploy.js
@@ -0,0 +1,28 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const factory = await hre.ethers.getContractFactory("SubnetTreasury");
+
+  const subnetTreasury = await factory.deploy(
+    "0x5bC5ea6E43425fa08A03ee7b5D1C1726057E7664"
+  );
+
+  await subnetTreasury.deployed();
+
+  console.log("SubnetTreasury deploy to ", subnetTreasury.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ applications/subswap/contract/scripts/subnettreasurydeploy.js
@@ -0,0 +1,26 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const factory = await hre.ethers.getContractFactory("ParentnetTreasury");
+
+  const parentnetTreasury = await factory.deploy("0xe974de4A684bb358Bc9141Cd0187010e998854F5");
+
+  await parentnetTreasury.deployed();
+
+  console.log("ParentnetTreasury deploy to ", parentnetTreasury.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
