b8e2ac12b4c604e6186b68fe1adf874e23b3b00d
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-05-28 11:00:11 +0400

    update

--- applications/subswap/contract/contracts/TreasuryToken.sol
+++ applications/subswap/contract/contracts/TreasuryToken.sol
@@ -8,9 +8,7 @@ contract TreasuryToken is ERC20Burnable, Ownable {
     constructor(
         string memory name_,
         string memory symbol_
-    ) ERC20(name_, symbol_) {
-        mint(msg.sender, 1 ether);
-    }
+    ) ERC20(name_, symbol_) {}
 
     function mint(address account, uint256 amount) public onlyOwner {
         _mint(account, amount);
