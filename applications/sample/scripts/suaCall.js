f581cdc9390dd3e5d6a8ac22a0c6424a4c54bc45
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-06-19 11:57:45 +0400

    sample contracts (#11)
    sample contracts

---
+++ applications/sample/scripts/suaCall.js
@@ -0,0 +1,26 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+const deploy = require("../deploy.config.json");
+
+async function main() {
+  const factory = await hre.ethers.getContractFactory("SimpleSua");
+
+  const sua = await factory.attach(deploy.sua);
+
+  await sua.simpleCall();
+  console.log("SimpleSua simpleCall success");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ applications/sample/scripts/suaDeploy.js
@@ -0,0 +1,27 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+
+async function main() {
+  const factory = await hre.ethers.getContractFactory("SimpleSua");
+
+  const parentnetTreasury = await factory.deploy();
+
+  await parentnetTreasury.deployed();
+
+  console.log("SimpleSua deploy to ", parentnetTreasury.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
