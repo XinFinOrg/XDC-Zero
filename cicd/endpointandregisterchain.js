e65af5d963b4ce86f1770ca4c9d70f1ba32ede15
wanwiset25 <wanwiset25@gmail.com>
2024-07-08 11:23:55 +0400

    code formatting

--- cicd/endpointandregisterchain.js
+++ cicd/endpointandregisterchain.js
@@ -1,37 +1,40 @@
-process.chdir(__dirname)
+process.chdir(__dirname);
 const { execSync } = require("child_process");
-const fs = require('node:fs');
-const env = require("dotenv").config({path: 'mount/.env'});
+const fs = require("node:fs");
+const env = require("dotenv").config({ path: "mount/.env" });
 const config = {
-  "relativePath": "../endpoint/"
+  relativePath: "../endpoint/",
+};
+const endpointConfig = {};
+
+const { ethers } = require("ethers");
+const u = require("./util.js");
+
+main();
+
+async function main() {
+  console.log("start endpoint deploy and register chain");
+  initEndpointDeploy();
+  await u.getNetworkID(config);
+  deployEndpoint();
+  configureEndpointJson();
+  registerEndpoint();
+  exportEndpointJson();
 }
-const endpointConfig = {}
 
-const { ethers } = require('ethers')
-const u = require('./util.js')
-
-main()
-
-async function main(){
-  console.log("start endpoint deploy and register chain")
-  initEndpointDeploy()
-  await u.getNetworkID(config)
-  deployEndpoint()
-  configureEndpointJson()
-  registerEndpoint()
-  exportEndpointJson()
-}
-
-function initEndpointDeploy(){
-  if (process.env.PARENTNET_URL){
-    parentnetURL = process.env.PARENTNET_URL
-  } else if (process.env.PARENTNET){
-    parentnet = process.env.PARENTNET
-    if (parentnet == "devnet") parentnetURL = "https://devnetstats.apothem.network/devnet";
-    if (parentnet == "testnet") parentnetURL = "https://devnetstats.apothem.network/testnet";
-    if (parentnet == "mainnet") parentnetURL = "https://devnetstats.apothem.network/mainnet";
+function initEndpointDeploy() {
+  if (process.env.PARENTNET_URL) {
+    parentnetURL = process.env.PARENTNET_URL;
+  } else if (process.env.PARENTNET) {
+    parentnet = process.env.PARENTNET;
+    if (parentnet == "devnet")
+      parentnetURL = "https://devnetstats.apothem.network/devnet";
+    if (parentnet == "testnet")
+      parentnetURL = "https://devnetstats.apothem.network/testnet";
+    if (parentnet == "mainnet")
+      parentnetURL = "https://devnetstats.apothem.network/mainnet";
   } else {
-    throw Error("PARENTNET or PARENTNET_URL not found")
+    throw Error("PARENTNET or PARENTNET_URL not found");
   }
 
   const reqENV = [
@@ -41,119 +44,143 @@ function initEndpointDeploy(){
     "CSC",
     "REVERSE_CSC",
   ];
-  const isEnabled = reqENV.every(envVar => envVar in process.env)
-  if (!isEnabled){
-    throw Error("incomplete ENVs, require SUBNET_PK, PARENTNET_PK, SUBNET_URL, CSC, REVERSE_CSC")
+  const isEnabled = reqENV.every((envVar) => envVar in process.env);
+  if (!isEnabled) {
+    throw Error(
+      "incomplete ENVs, require SUBNET_PK, PARENTNET_PK, SUBNET_URL, CSC, REVERSE_CSC"
+    );
   }
-  subnetPK = process.env.SUBNET_PK.startsWith("0x") ? process.env.SUBNET_PK : `0x${process.env.SUBNET_PK}`;
-  parentnetPK = process.env.PARENTNET_PK.startsWith("0x") ? process.env.PARENTNET_PK : `0x${process.env.PARENTNET_PK}`;
-  csc = process.env.CSC.startsWith("0x") ? process.env.CSC : `0x${process.env.CSC}`;
-  reverseCSC = process.env.REVERSE_CSC.startsWith("0x") ? process.env.REVERSE_CSC : `0x${process.env.REVERSE_CSC}`;
+  subnetPK = process.env.SUBNET_PK.startsWith("0x")
+    ? process.env.SUBNET_PK
+    : `0x${process.env.SUBNET_PK}`;
+  parentnetPK = process.env.PARENTNET_PK.startsWith("0x")
+    ? process.env.PARENTNET_PK
+    : `0x${process.env.PARENTNET_PK}`;
+  csc = process.env.CSC.startsWith("0x")
+    ? process.env.CSC
+    : `0x${process.env.CSC}`;
+  reverseCSC = process.env.REVERSE_CSC.startsWith("0x")
+    ? process.env.REVERSE_CSC
+    : `0x${process.env.REVERSE_CSC}`;
   subnetURL = process.env.SUBNET_URL;
 
-  // return subnetURL, parentnetURL, subnetPK, parentnetPK, csc, reverseCSC 
-  config["subnetPK"] = subnetPK
-  config["parentnetPK"] = parentnetPK
-  config["subnetURL"] = subnetURL
-  config["parentnetURL"] = parentnetURL
-  config["csc"] = csc
-  config["reverseCSC"] = reverseCSC
-  
+  // return subnetURL, parentnetURL, subnetPK, parentnetPK, csc, reverseCSC
+  config["subnetPK"] = subnetPK;
+  config["parentnetPK"] = parentnetPK;
+  config["subnetURL"] = subnetURL;
+  config["parentnetURL"] = parentnetURL;
+  config["csc"] = csc;
+  config["reverseCSC"] = reverseCSC;
 }
 
-function configureEndpointJson(){
-  endpointConfig["xdcsubnet"]={
-    "endpoint": config.subnetEndpoint,
-    "registers":[
+function configureEndpointJson() {
+  endpointConfig["xdcsubnet"] = {
+    endpoint: config.subnetEndpoint,
+    registers: [
       {
-        "csc": config.reverseCSC,
-        "endpoint": config.parentnetEndpoint,
-        "chainId": config.parentnetID
-      }
+        csc: config.reverseCSC,
+        endpoint: config.parentnetEndpoint,
+        chainId: config.parentnetID,
+      },
     ],
-    "applications":[]
-  }
-  endpointConfig["xdcparentnet"]={
-    "endpoint": config.parentnetEndpoint,
-    "registers":[
+    applications: [],
+  };
+  endpointConfig["xdcparentnet"] = {
+    endpoint: config.parentnetEndpoint,
+    registers: [
       {
-        "csc": config.csc,
-        "endpoint": config.subnetEndpoint,
-        "chainId": config.subnetID
-      }
+        csc: config.csc,
+        endpoint: config.subnetEndpoint,
+        chainId: config.subnetID,
+      },
     ],
-    "applications":[]
+    applications: [],
+  };
+
+  console.log("writing endpointconfig.json");
+  fs.writeFileSync(
+    "../endpoint/endpointconfig.json",
+    JSON.stringify(endpointConfig, null, 2),
+    "utf-8",
+    (err) => {
+      if (err) {
+        throw Error("error writing endpointconfig.json, " + err);
+      }
+    }
+  );
+}
+
+function exportEndpointJson() {
+  fs.copyFileSync(
+    "../endpoint/endpointconfig.json",
+    "./mount/endpointconfig.json"
+  );
+  ep = fs.readFileSync("../endpoint/endpointconfig.json").toString();
+  console.log("SUCCESS deploy endpoint and register chain, endpointconfig:");
+  console.log(ep);
+  console.log();
+  console.log("SUCCESS deploy endpoint and register chain, env:");
+  console.log("SUBNET_ZERO_CONTRACT=" + config.subnetEndpoint);
+  console.log("PARENTNET_ZERO_CONTRACT=" + config.parentnetEndpoint);
+}
+
+function deployEndpoint() {
+  console.log("writing network config");
+  u.writeNetworkJson(config);
+  console.log("configuring PK");
+  u.writeEnv(config.subnetPK, config.relativePath);
+  console.log("deploying subnet endpoint");
+  subnetEndpointOut = u.callExec(
+    "cd ../endpoint; npx hardhat run scripts/endpointdeploy.js --network xdcsubnet"
+  );
+  subnetZeroEndpoint = parseEndpointOutput(subnetEndpointOut);
+
+  console.log("configuring PK");
+  u.writeEnv(config.parentnetPK, config.relativePath);
+  console.log("deploying parentnet endpoint");
+  parentnetEndpointOut = u.callExec(
+    "cd ../endpoint; npx hardhat run scripts/endpointdeploy.js --network xdcparentnet"
+  );
+  parentnetZeroEndpoint = parseEndpointOutput(parentnetEndpointOut);
+
+  config["subnetEndpoint"] = subnetZeroEndpoint;
+  config["parentnetEndpoint"] = parentnetZeroEndpoint;
+}
+
+function registerEndpoint() {
+  console.log("writing network config");
+  u.writeNetworkJson(config);
+  console.log("configuring PK");
+  u.writeEnv(config.subnetPK, config.relativePath);
+  console.log("register parentnet to subnet endpoint");
+  subnetEndpointOut = u.callExec(
+    "cd ../endpoint; npx hardhat run scripts/registerchain.js --network xdcsubnet"
+  );
+  if (!subnetEndpointOut.includes("success"))
+    throw Error("failed to register parentnet endpoint to subnet");
+
+  console.log("configuring PK");
+  u.writeEnv(config.parentnetPK, config.relativePath);
+  console.log("register subnet to parentnet endpoint");
+  parentnetEndpointOut = u.callExec(
+    "cd ../endpoint; npx hardhat run scripts/registerchain.js --network xdcparentnet"
+  );
+  if (!parentnetEndpointOut.includes("success"))
+    throw Error("failed to register subnet endpoint to parentnet");
+}
+
+function parseEndpointOutput(outString) {
+  strArr = outString.split("\n");
+  lastLine = strArr[strArr.length - 1];
+  if (lastLine == "") {
+    strArr.pop();
+    lastLine = strArr[strArr.length - 1];
   }
-  
-  console.log("writing endpointconfig.json")
-  fs.writeFileSync('../endpoint/endpointconfig.json', JSON.stringify(endpointConfig, null, 2) , 'utf-8', err => {
-    if (err) {
-      throw Error("error writing endpointconfig.json, "+err)
-    } 
-  });
-}
-
-function exportEndpointJson(){
-  fs.copyFileSync('../endpoint/endpointconfig.json', './mount/endpointconfig.json')
-  ep = fs.readFileSync('../endpoint/endpointconfig.json').toString()
-  console.log("SUCCESS deploy endpoint and register chain, endpointconfig:")
-  console.log(ep)
-  console.log()
-  console.log("SUCCESS deploy endpoint and register chain, env:")
-  console.log("SUBNET_ZERO_CONTRACT="+config.subnetEndpoint)
-  console.log("PARENTNET_ZERO_CONTRACT="+config.parentnetEndpoint)
-}
-
-function deployEndpoint(){
-  console.log("writing network config")
-  u.writeNetworkJson(config)
-  console.log("configuring PK")
-  u.writeEnv(config.subnetPK, config.relativePath)
-  console.log("deploying subnet endpoint")
-  subnetEndpointOut = u.callExec("cd ../endpoint; npx hardhat run scripts/endpointdeploy.js --network xdcsubnet")
-  subnetZeroEndpoint = parseEndpointOutput(subnetEndpointOut)
-
-  console.log("configuring PK")
-  u.writeEnv(config.parentnetPK, config.relativePath)
-  console.log("deploying parentnet endpoint")
-  parentnetEndpointOut = u.callExec("cd ../endpoint; npx hardhat run scripts/endpointdeploy.js --network xdcparentnet")
-  parentnetZeroEndpoint = parseEndpointOutput(parentnetEndpointOut)
-
-  config["subnetEndpoint"] = subnetZeroEndpoint
-  config["parentnetEndpoint"] = parentnetZeroEndpoint
-}
-
-function registerEndpoint(){
-  console.log("writing network config")
-  u.writeNetworkJson(config)
-  console.log("configuring PK")
-  u.writeEnv(config.subnetPK, config.relativePath)
-  console.log("register parentnet to subnet endpoint")
-  subnetEndpointOut = u.callExec("cd ../endpoint; npx hardhat run scripts/registerchain.js --network xdcsubnet")
-  if (!subnetEndpointOut.includes("success")) throw Error("failed to register parentnet endpoint to subnet")
-
-  console.log("configuring PK")
-  u.writeEnv(config.parentnetPK, config.relativePath)
-  console.log("register subnet to parentnet endpoint")
-  parentnetEndpointOut = u.callExec("cd ../endpoint; npx hardhat run scripts/registerchain.js --network xdcparentnet")
-  if (!parentnetEndpointOut.includes("success")) throw Error("failed to register subnet endpoint to parentnet")
-
-}
-
-function parseEndpointOutput(outString){
-  strArr = outString.split("\n") 
-  lastLine = strArr[strArr.length-1]
-  if (lastLine == ''){
-    strArr.pop()
-    lastLine = strArr[strArr.length-1]
-  }
-  if (lastLine.startsWith("XDCZeroEndpoint")){
-    idx = lastLine.indexOf("0x")
-    address = lastLine.slice(idx, idx+42)
-    return address  
+  if (lastLine.startsWith("XDCZeroEndpoint")) {
+    idx = lastLine.indexOf("0x");
+    address = lastLine.slice(idx, idx + 42);
+    return address;
   } else {
-    throw Error("invalid output string: "+outString)
+    throw Error("invalid output string: " + outString);
   }
 }
-
-
--- cicd/util.js
+++ cicd/util.js
@@ -1,79 +1,79 @@
-process.chdir(__dirname)
+process.chdir(__dirname);
 const { execSync } = require("child_process");
-const fs = require('node:fs');
-const env = require("dotenv").config({path: 'mount/.env'});
+const fs = require("node:fs");
+const env = require("dotenv").config({ path: "mount/.env" });
 
-const { ethers } = require('ethers')
+const { ethers } = require("ethers");
 
-function writeEnv(key, path){
-  content = "PRIVATE_KEY="+key
-  fullPath = path+"/"+".env"
-  fs.writeFileSync(fullPath, content, err => {
+function writeEnv(key, path) {
+  content = "PRIVATE_KEY=" + key;
+  fullPath = path + "/" + ".env";
+  fs.writeFileSync(fullPath, content, (err) => {
     if (err) {
-      throw Error(`error writing ${fullPath}, `+err)
+      throw Error(`error writing ${fullPath}, ` + err);
     }
   });
 }
-function writeNetworkJson(config){
+function writeNetworkJson(config) {
   networkJson = {
-    "xdcsubnet": config.subnetURL,
-    "xdcparentnet": config.parentnetURL,
-  }
-  writeJson(networkJson, config.relativePath, "network.config.json")
+    xdcsubnet: config.subnetURL,
+    xdcparentnet: config.parentnetURL,
+  };
+  writeJson(networkJson, config.relativePath, "network.config.json");
 }
 
-function writeJson(obj, path, filename){
-  fullPath = path+"/"+filename
-  fs.writeFileSync(fullPath, JSON.stringify(obj, null, 2) , 'utf-8', err => {
+function writeJson(obj, path, filename) {
+  fullPath = path + "/" + filename;
+  fs.writeFileSync(fullPath, JSON.stringify(obj, null, 2), "utf-8", (err) => {
     if (err) {
-      throw Error(`error writing ${fullPath}, `+err)
-    } 
+      throw Error(`error writing ${fullPath}, ` + err);
+    }
   });
 }
 
-function callExec(command){
-  try{
-    const stdout = execSync(command,{timeout: 200000})
+function callExec(command) {
+  try {
+    const stdout = execSync(command, { timeout: 200000 });
     // const stdout = execSync(command)
-    output = stdout.toString()
+    output = stdout.toString();
     // console.log(`${stdout}`);
     console.log(output);
-    return output
-  } catch (error){
+    return output;
+  } catch (error) {
     if (error.code) {
       // Spawning child process failed
-      if (error.code == "ETIMEDOUT"){
-        throw Error("Timed out (200 seconds)")
+      if (error.code == "ETIMEDOUT") {
+        throw Error("Timed out (200 seconds)");
       } else {
-        throw Error(error)
+        throw Error(error);
       }
     } else {
       // Child was spawned but exited with non-zero exit code
       // Error contains any stdout and stderr from the child
       // const { stdout, stderr } = error;
       // console.error({ stdout, stderr });
-      throw Error(error)
+      throw Error(error);
     }
   }
 }
 
-async function getNetworkID(config){
-  subnetURL = config.subnetURL
-  parentnetURL = config.parentnetURL
-  console.log("getting chain ID")
-  const subRPC = new ethers.providers.JsonRpcProvider(subnetURL)
-  subID = await subRPC.getNetwork()
-  subID = subID.chainId.toString()
-  console.log("subnet chain ID:", subID)
-  
-  const parentRPC = new ethers.providers.JsonRpcProvider(parentnetURL)
-  parentID = await parentRPC.getNetwork()
-  parentID = parentID.chainId.toString()
-  console.log("parentnet chain ID:", parentID)
-  console.log()
+async function getNetworkID(config) {
+  subnetURL = config.subnetURL;
+  parentnetURL = config.parentnetURL;
+  console.log("getting chain ID");
+  const subRPC = new ethers.providers.JsonRpcProvider(subnetURL);
+  subID = await subRPC.getNetwork();
+  subID = subID.chainId.toString();
+  console.log("subnet chain ID:", subID);
 
-  config["subnetID"] = subID
-  config["parentnetID"] = parentID
+  const parentRPC = new ethers.providers.JsonRpcProvider(parentnetURL);
+  parentID = await parentRPC.getNetwork();
+  parentID = parentID.chainId.toString();
+  console.log("parentnet chain ID:", parentID);
+  console.log();
+
+  config["subnetID"] = subID;
+  config["parentnetID"] = parentID;
 }
 
 module.exports = {
@@ -81,4 +81,4 @@ module.exports = {
   callExec,
   writeEnv,
   writeNetworkJson,
-}
+};
